a3514f5a97086edc868bebc57a1fee963443bd7b
apgamin <ap3911631@gmail.com>
2026-07-15 13:52:41 +0530

    Enable SPA mode

--- app/root.tsx
+++ app/root.tsx
@@ -7,7 +7,9 @@ import {
   ScrollRestoration,
 } from "react-router";
 
+import { useEffect } from "react";
 import type { Route } from "./+types/root";
+import { usePuterStore } from "~/lib/puter";
 import "./app.css";
 
 export const links: Route.LinksFunction = () => [
@@ -25,23 +27,33 @@ export const links: Route.LinksFunction = () => [
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+      <html lang="en">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
+
       <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
+      {children}
+      <ScrollRestoration />
+      <Scripts />
+
+      {/* Load Puter SDK */}
+      <script src="https://js.puter.com/v2/"></script>
       </body>
-    </html>
+      </html>
   );
 }
 
 export default function App() {
+  const { init } = usePuterStore();
+
+  useEffect(() => {
+    init();
+  }, [init]);
+
   return <Outlet />;
 }
 
@@ -53,23 +65,24 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
   if (isRouteErrorResponse(error)) {
     message = error.status === 404 ? "404" : "Error";
     details =
-      error.status === 404
-        ? "The requested page could not be found."
-        : error.statusText || details;
-  } else if (import.meta.env.DEV && error && error instanceof Error) {
+        error.status === 404
+            ? "The requested page could not be found."
+            : error.statusText || details;
+  } else if (import.meta.env.DEV && error instanceof Error) {
     details = error.message;
     stack = error.stack;
   }
 
   return (
-    <main className="pt-16 p-4 container mx-auto">
-      <h1>{message}</h1>
-      <p>{details}</p>
-      {stack && (
-        <pre className="w-full p-4 overflow-x-auto">
+      <main className="pt-16 p-4 container mx-auto">
+        <h1>{message}</h1>
+        <p>{details}</p>
+
+        {stack && (
+            <pre className="w-full overflow-x-auto p-4">
           <code>{stack}</code>
         </pre>
-      )}
-    </main>
+        )}
+      </main>
   );
 }
